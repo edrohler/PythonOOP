1b631205677643e0271507560a2075b2cc9c23e4
Eric D. Rohler <erohler@wearemoore.com>
2024-01-02 11:53:59 -0500

    Update UOW and Tests

--- src/infrastructure/unit_of_work.py
+++ src/infrastructure/unit_of_work.py
@@ -1,15 +1,20 @@
+from src.core.services.logging_service import LoggingService
 from .database import get_session, DatabaseConfig
 from .repositories import AddressRepository, EmailRepository, PersonRepository
 
 class UnitOfWork:
-    def __init__(self, config: DatabaseConfig):
+    def __init__(self, config: DatabaseConfig, logger: LoggingService):
+        self.logger = logger
+        logger.log_info('Creating new unit of work')
         self.session = config.get_session()
         self.address_repository = AddressRepository(self.session, logger=config.logger)
         self.email_repository = EmailRepository(self.session, logger=config.logger)
         self.person_repository = PersonRepository(self.session, logger=config.logger)
         
     def __enter__(self):
+        self.logger.log_info('Entering unit of work')
         return self
     
     def __exit__(self, type, value, traceback):
+        self.logger.log_info('Exiting unit of work')
         self.session.close()
--- tests/infrastructure/test_unit_of_work.py
+++ tests/infrastructure/test_unit_of_work.py
@@ -3,10 +3,10 @@ from unittest.mock import MagicMock, patch
 from src.infrastructure.unit_of_work import UnitOfWork
 from src.infrastructure.repositories import AddressRepository, EmailRepository, PersonRepository
 
-def test_unit_of_work(test_session, mock_database_config):
+def test_unit_of_work(test_session, mock_database_config, mock_logger):
     # Act
     mock_database_config.get_session = MagicMock(return_value=test_session)
-    uow = UnitOfWork(mock_database_config)
+    uow = UnitOfWork(mock_database_config, mock_logger)
     
     # Assert
     assert isinstance(uow.address_repository, AddressRepository)
@@ -14,14 +14,12 @@ def test_unit_of_work(test_session, mock_database_config):
     assert isinstance(uow.person_repository, PersonRepository)
     assert uow.session is test_session
 
-def test_unit_of_work_session_close(test_session, mock_database_config):
+def test_unit_of_work_session_close(test_session, mock_database_config, mock_logger):
     # Act
-    with patch('src.infrastructure.database.get_session', return_value=test_session), \
-         patch.object(test_session, 'close') as mock_close, \
-         patch.object(UnitOfWork, '__exit__') as mock_exit:
-        with UnitOfWork(mock_database_config) as uow:
+    with patch('src.infrastructure.database.DatabaseConfig.get_session', return_value=test_session), \
+         patch.object(UnitOfWork, '__exit__') as mock_exit: 
+        with UnitOfWork(config=mock_database_config, logger=mock_logger) as uow:
             pass
     
     # Assert
     mock_exit.assert_called_once()
-    # mock_close.assert_called_once()
