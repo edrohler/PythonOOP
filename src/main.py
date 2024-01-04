780791fbd1acd3535c037d12aa9ad76154d0828f
Eric D. Rohler <erohler@wearemoore.com>
2024-01-04 09:31:48 -0500

    Adjust the launch.json

--- src/main.py
+++ src/main.py
@@ -8,11 +8,14 @@ from api.app import app
 def main():
     parser = argparse.ArgumentParser(description="Start the Flask app")
     parser.add_argument("--serve", action="store_true", help="Start the Flask API App")
+    parser.add_argument("--debug", action="store_true", help="Set debug mode")
 
     args = parser.parse_args()
 
     if args.serve:
-        app.run(debug=True, use_reloader=False)
+        app.run(debug=False, use_reloader=False)
+    elif args.serve and args.debug:
+        app.run(debug=True, use_reloader=True)
 
 if __name__ == "__main__":
     main()
