3b42ecea959fda51ad6f34764624e4b349c1f723
Eric D. Rohler <edrohler@ericrohler.com>
2024-01-07 19:19:21 -0500

    Add CreatedAt, By & UpdatedAt, By Fields

--- src/infrastructure/orm/entities/base_entity.py
+++ src/infrastructure/orm/entities/base_entity.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -6,3 +6,7 @@ Base = declarative_base()
 class BaseEntity(Base):
     __abstract__ = True
     id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, nullable=False)
+    created_by = Column(String, nullable=False)
+    updated_at = Column(DateTime, nullable=True)
+    updated_by = Column(String, nullable=True)
